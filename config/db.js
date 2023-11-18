f740e2e1f9b78d666263bfad044b530e6a1016c2
baldez300 <baldez111990@gmail.com>
2023-11-18 15:58:33 +0200

    blog3 excercise

--- config/db.js
+++ config/db.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
-const MONGO_URI =
-  "mongodb+srv://tx00-web:TX00%23web%2323@cluster0.1x4ks.mongodb.net/2023-11-17";
+require('dotenv').config(); // Load environment variables from .env
+
+const MONGO_URI = process.env.MONGO_URI; // Access the database URI from .env
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(MONGO_URI);
-  console.log(`Connected to database`);
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB is connected!");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+  
 };
 
 module.exports = connectDB;
